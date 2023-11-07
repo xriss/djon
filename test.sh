604c7a7fb19624ffbfed20b0fdd2cb0fc876e64f
kriss <kriss@xixs.com>
2023-11-07 01:00:20 +0000

    test output

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd `dirname $0`
+cd test
+
+echo " Converting all input json "
+
+echo "" >output.json
+for fname in `find json -name "*.json" -type f -exec echo {} \;` ; do
+
+	echo Transforming ${fname}
+	
+	echo "" >>output.json
+	echo "#$fname" >>output.json
+	cat "$fname" >>output.json
+
+	echo "" >>output.json
+	echo "#c" >>output.json
+	{ ../c/main "${fname}"; } &>>output.json
+
+done
+
+
+
