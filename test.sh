ff298dc07b6e8b513bfa0cc7926b25e7024abf23
kriss <kriss@xixs.com>
2023-12-16 14:18:50 +0000

    replace non 7bit ascii with #

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 cd `dirname $0`
 cd test
 
-echo "# Converting all input json ( note this file is forced 7bit ascii clean so not exact output )" >output.json
+echo "# Converting all input json ( note this file is forced 7bit ascii clean with # replacing the missing bytes )" >output.json
 
 files=`find json -name "*.json" -type f -exec echo {} \; && find djon -name "*.djon" -type f -exec echo {} \;`
 
@@ -14,21 +14,21 @@ for fname in $files ; do
 	
 	echo "" >>output.json
 	echo "#$fname" >>output.json
-	{ cat "$fname" | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ cat "$fname" | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 
 	echo "" >>output.json
 	echo "#c.json" >>output.json
-	{ ../c/djon "${fname}" | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ ../c/djon "${fname}" | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 	echo "#c.djon" >>output.json
-	{ ../c/djon --djon "${fname}" | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ ../c/djon --djon "${fname}" | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 	echo "#lua.json" >>output.json
-	{ luajit -- ../lua/djon.cmd.lua "${fname}" 2>&1 | sed -n '/stack traceback:/q;p' | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ luajit -- ../lua/djon.cmd.lua "${fname}" 2>&1 | sed -n '/stack traceback:/q;p' | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 	echo "#lua.djon" >>output.json
-	{ luajit -- ../lua/djon.cmd.lua --djon "${fname}" 2>&1 | sed -n '/stack traceback:/q;p' | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ luajit -- ../lua/djon.cmd.lua --djon "${fname}" 2>&1 | sed -n '/stack traceback:/q;p' | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 	echo "#js.json" >>output.json
-	{ node  -- ../js/djon_cmd.js "${fname}" 2>&1 | sed -n '/at djon.check_error/q;p' | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ node  -- ../js/djon_cmd.js "${fname}" 2>&1 | sed -n '/at djon.check_error/q;p' | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 	echo "#js.djon" >>output.json
-	{ node  -- ../js/djon_cmd.js --djon "${fname}" 2>&1 | sed -n '/at djon.check_error/q;p' | tr -cd '[:print:]\t\n\r' ; } &>>output.json
+	{ node  -- ../js/djon_cmd.js --djon "${fname}" 2>&1 | sed -n '/at djon.check_error/q;p' | tr -c '[:print:]\t\n\r' '#' ; } &>>output.json
 
 
 done
