387d1aa798d4c0cab224d6114e7aa86e37e1f670
kriss <kriss@xixs.com>
2024-10-16 03:10:30 +0100

    lua segfault? hard to recreate

--- lua/djon.sh
+++ lua/djon.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 eval `luarocks --lua-version 5.1 path`
-gdb --args luajit -- `dirname $0`/djon.cmd.lua "$@"
+gdb -q --eval-command=r --eval-command=bt --eval-command=q --args luajit -- `dirname $0`/djon.cmd.lua "$@"
